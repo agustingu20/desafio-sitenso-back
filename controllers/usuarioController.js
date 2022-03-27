39330e394591e1f9f93d9e0c7e890a4b2212f8a8
Agustin Gomez Urrutia <agustin.gu20@gmail.com>
2022-03-27 11:36:55 -0300

    Delete the crearUsuario code from usuarioController because it wasn't used

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -2,40 +2,6 @@ const Usuario = require("../models/UsuarioModel")
 const bcryptjs = require('bcryptjs')
 const { validationResult } = require("express-validator")
 
-
-// exports.crearUsuario = async (req, res) => {
-//     const { email, password } = req.body
-//     try {
-
-//         //Se revisan errores
-//         const errores = validationResult(req)
-//         if (!errores.isEmpty())
-//             return res.status(400).json({ msg: errores.array() })
-
-//         //Revisando Email
-//         let usuario = await Usuario.findOne({ email })
-
-//         if (usuario) {
-//             return res.status(400).json({ msg: "El usuario ya existe" })
-//         }
-
-//         //Nuevo usuario
-//         usuario = new Usuario(req.body)
-
-//         //Hashear password
-//         const salt = await bcryptjs.genSalt(10)
-//         usuario.password = await bcryptjs.hash(password, salt)
-
-//         //Guardar usuario
-//         await usuario.save()
-
-//         res.send("Usuario creado correctamente")
-//     } catch (error) {
-//         console.log(error)
-//         res.status(400).send("Hubo un error al crear al usuario")
-//     }
-// }
-
 exports.obtenerUsuarios = async (req, res) => {
     try {
         const usuarios = await Usuario.find();
